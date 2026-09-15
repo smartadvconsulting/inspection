ea077ed023bfd98d65a71072c0466c2600449cf6
smartadvconsulting <chad.ntip@gmail.com>
2026-09-16 00:27:08 +1200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// 99 Oakland Hill inspection form settings.
+// Paste the Web app URL from Apps Script (Deploy > Manage deployments) between the quotes. It ends in /exec.
+window.INSPECTION_API_URL = 'PASTE_WEB_APP_URL_HERE';
