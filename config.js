09c2041536b3162e54c6f3c0236fd16fc22ccaa0
smartadvconsulting <chad.ntip@gmail.com>
2026-09-16 00:27:58 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 99 Oakland Hill inspection form settings.
 // Paste the Web app URL from Apps Script (Deploy > Manage deployments) between the quotes. It ends in /exec.
-window.INSPECTION_API_URL = 'PASTE_WEB_APP_URL_HERE';
+window.INSPECTION_API_URL = 'https://script.google.com/macros/s/AKfycbwTNZWz69Boi7N_E8FhyL2nrN1WjxsFBxLaZ8z6pXavyg9HHlLghCYLGkv_Gz25HH1B/exec';
